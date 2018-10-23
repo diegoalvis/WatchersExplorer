1985ae3bc645739dda173d1a7f598c95b8c9d5fc
Diego Alvis <diego.x.alvis.-nd@disney.com>
2018-10-23 00:34:43 -0500

    Fetch data from API service using view model component and rx

--- app/src/main/java/com/example/diegoalvis/watchersexplorer/MainActivity.kt
+++ app/src/main/java/com/example/diegoalvis/watchersexplorer/MainActivity.kt
@@ -1,38 +1,49 @@
 package com.example.diegoalvis.watchersexplorer
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.diegoalvis.watchersexplorer.viewmodels.SharedViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  viewModel: SharedViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
         }
+
+        viewModel = ViewModelProviders.of(this).get(SharedViewModel::class.java)
+        viewModel.repos.observe(this, Observer {
+            // TODO: Add items to adapter
+            Log.e("Alvis", it.size.toString())
+        })
     }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel.searchRepositories()
+    }
+
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+                R.id.action_settings -> true
+                else -> super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/diegoalvis/watchersexplorer/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/example/diegoalvis/watchersexplorer/viewmodels/SharedViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.diegoalvis.watchersexplorer.viewmodels
 
+import android.annotation.SuppressLint
+import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.diegoalvis.android.newsapp.api.ApiClient
-import com.diegoalvis.android.newsapp.api.SearchResponse
 import com.example.diegoalvis.watchersexplorer.models.Repo
 import io.reactivex.Flowable
+import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class SharedViewModel : ViewModel() {
-//    var isLoading: ObservableBoolean = ObservableBoolean(false)
+
+    var isLoading: ObservableBoolean = ObservableBoolean(false)
 
     private val apiInterface = ApiClient.getInterface()
+    private var lastKeyWordSearched: String? = null
 
     val repos = MutableLiveData<List<Repo>>()
     val selected = MutableLiveData<Repo>()
@@ -21,12 +26,21 @@ class SharedViewModel : ViewModel() {
         selected.value = item
     }
 
-    fun searchRepositories(keyWords: String): Flowable<SearchResponse>? {
-//        isLoading.set(true)
-        return apiInterface
-            .searchRepos(keyWords)
-//            .doOnTerminate { isLoading.set(false) }
-            .onBackpressureBuffer()
-            .observeOn(AndroidSchedulers.mainThread())
+    @SuppressLint("CheckResult")
+    fun searchRepositories(keyWord: String = "tetris") {
+        if (keyWord != lastKeyWordSearched) {
+            repos.value = arrayListOf()
+        }
+
+        isLoading.set(true)
+        apiInterface
+            .searchRepos(keyWord)
+            .applyUISchedulers()
+            .doOnTerminate { isLoading.set(false) }
+            .subscribe { repos.value = it.items }
     }
 }
+
+private fun <T> Flowable<T>.applyUISchedulers()
+        = this.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
